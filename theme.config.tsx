bc626f32c91bf1ce71e0d70aba8d71f8e8ca4eb6
Brett Lamy <brettlamy@gmail.com>
2023-04-03 00:27:28 -0700

    first draft

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>NextJS.ai</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/blamy/nextjs-ai-starter',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/2F2bHSma',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/BLamy/docs.nextjs.ai',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'MIT License',
   },
 }
 
